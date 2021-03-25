51287b7db66c03b48b5bd30db5c1bf0fdd372c70
mbrown <tugtuj@gmail.com>
2021-03-25 00:09:02 -0400

    Form add

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('water/', views.water_index, name='water'),
-    path('water/<int:water_id>/', views.water_show, name='water_show')
+    path('water/<int:water_id>/', views.water_show, name='water_show'),
+    path('water/create/', views.WaterCreate.as_view(), name='water_create'),
+    path('water/<int:pk>/update/', views.WaterUpdate.as_view(), name='water_update'),
+    path('water/<int:pk>/delete/', views.WaterDelete.as_view(), name='water_delete')
+
 ] 
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Waters
 from django.http import HttpResponse
+
+class WaterCreate(CreateView):
+    model = Waters
+    fields = '__all__'
+    success_url = '/water'
+
+class WaterUpdate(UpdateView):
+    model = Waters
+    fields = ['brand', 'sizeoz', 'watertype']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+        return HttpResponseRedirect('/water/' + str(self.object.pk))
+
+class WaterDelete(DeleteView):
+    model = Waters
+    success_url = '/water'
 
 def index(request):
     return HttpResponse('<h3>Detroit says: What up Doe? Drink some water.</h3>')
